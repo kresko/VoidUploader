1dce82f2872e2be6bfc55edf4733e39e475427b8
kresko <kresko1995@gmail.com>
2025-01-10 20:50:44 +0100

    [VU-2] - Added file uploading functionality.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,5 +3,7 @@ const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 
 indexRouter.get('/', indexController.renderHomepage);
+indexRouter.get('/upload-form', indexController.renderUploadForm);
+indexRouter.post('/upload', indexController.uploadFile);
 
 module.exports = indexRouter;
