6cd0cd40473df2e496fafc206c4d2d24c383e028
kresko <kresko1995@gmail.com>
2025-01-18 19:46:57 +0100

    [VU-2] - Added file uploading functionality.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,28 +1,30 @@
-const upload = require("../upload");
+const { upload, uploadMiddleware } = require("../upload");
 const db = require("../db/queryHandler");
 const { mkdir, rm } = require('fs');
 
 async function renderHomepage(req, res) {
     const userId = req.user ? req.user.id : 0;
     const folders = await db.getAllFoldersByUserId(userId);
+    const files = await db.getAllFiles();
     const error = req.query.error;
 
-    res.render('index', { user: req.user, folders: folders, error: error });
+    res.render('index', { user: req.user, folders: folders, files: files, error: error });
 }
 
 async function renderUploadForm(req, res) {
     res.render('upload', { user: req.user });
 }
 
-const uploadFile = (req, res) => {
-    upload.single('file')(req, res, (err) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send('File upload failed');
-        }
-
-        return res.redirect('/');
-    });
+const uploadFile = async (req, res) => {
+    try {
+        const folder = await db.getFolderByName(req.params.id);
+        await uploadMiddleware(req, res);
+        await db.insertNewFile(req.file, folder.id);
+        res.redirect('/');
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(error.message);
+    }
 };
 
 async function  createNewFolder(req, res) {
@@ -32,7 +34,6 @@ async function  createNewFolder(req, res) {
         mkdir(`./uploads/${req.body.folderName}`, { recursive: true }, (err) => {
             if (err) throw err;
         });
-        //add folder to filesystem fs.mkdir fs.rmdir
     } catch(e) {
         if (e.code === 'P2002') {
             return res.redirect('/?error=duplicated-folder');
@@ -53,10 +54,29 @@ async function deleteFolder(req, res) {
     res.redirect('/');
 }
 
+//TODO: finish functionality
+async function downloadFile(req, res) {
+
+}
+
+async function deleteFile(req, res) {
+    const file = await db.getFileById(req.params.id);
+    const folder = await db.getFolderNameById(file.folderId);
+    await db.deleteFile(req.params.id);
+
+    rm(`./uploads/${folder.name}/${file.name}`, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+    res.redirect('/');
+}
+
 module.exports = {
     renderHomepage,
     renderUploadForm,
     uploadFile,
     createNewFolder,
-    deleteFolder
+    deleteFolder,
+    downloadFile,
+    deleteFile
 };
--- upload.js
+++ upload.js
@@ -3,14 +3,30 @@ const multer = require('multer');
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, `uploads/${req.params.id}/`);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
     }
 });
 
+const uploadMiddleware = (req, res) => {
+    return new Promise((resolve, reject) => {
+        upload.single('file')(req, res, (err) => {
+            if (err) {
+                console.log(err);
+                reject(new Error('File upload failed'));
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
 // Create the multer instance
 const upload = multer({ storage: storage });
 
-module.exports = upload;
+module.exports = {
+    upload,
+    uploadMiddleware
+};
